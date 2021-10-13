90900adaf13508a42b87326befb775edeaaccf3e
RynoRyne <rynebrown88@gmail.com>
2021-10-13 01:08:39 +0000

    personal project

---
+++ personalProjects/pong.py
@@ -0,0 +1,11 @@
+import turtle as t
+playerAscore=0
+playerBscore=0
+
+#creats a window and declares a variable caled window and call the screen()
+window=t.screen()
+window.title("The Pong Game")
+window.bgcolor("green")
+window.setup(width=800,height=600)
+window.tracer(0)
+
