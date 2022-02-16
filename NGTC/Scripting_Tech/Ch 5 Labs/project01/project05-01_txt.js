fce28549e9188e41d29a3bad1f2c41a23336992b
rynebrown88@gmail.com <rynebrown88@gmail.com>
2022-02-16 10:01:11 -0500

    started project 5-1

--- NGTC/Scripting_Tech/Ch 5 Labs/project01/project05-01_txt.js
+++ NGTC/Scripting_Tech/Ch 5 Labs/project01/project05-01_txt.js
@@ -4,14 +4,16 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Ryne Brown
+      Date:   2/16/2022
 
       Filename: project05-01.js
 */
 
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
+var timeID;
+var questionList = document.querySelectorAll("div#quiz input");
 const quizTime = 20;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
@@ -26,7 +28,28 @@ let timeLeft = quizTime;
 
 // Declare the ID for timed commands
 // and the node list for questions
+startQuiz.onclick = function(){
+   overlay.setAttribute.className = "showquiz";
+   timeID= window.setInterval(countdown(), 1000);
+}
 
+function countdown(){
+   if (timeID = 0){
+      window.clearInterval(timeID);
+      let totalCorrect = checkAnswers;
+      if (totalCorrect = correctAnswers.length){
+         window.alert("Congrats on 100%");
+      } else {
+         window.alert("You got " + 5-correctAnswers + " out of 5 incorrect.");
+         timeLeft=quizTime;
+         quizClock.value = timeLeft;
+         overlay.setAttribute.className = "hidequiz";
+      }
+   } else {
+      timeID -= 1;
+   quizClock.value = timeLeft;
+   }
+}
 
 
 
